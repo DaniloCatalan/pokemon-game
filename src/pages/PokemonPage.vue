57056e200e46a6413638d12e801f862858cbe107
Danilo P Catalan <danilo.e.catalan@gmail.com>
2022-02-18 02:21:36 -0300

    add: components structure

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>¿Quién es este pokemon?</h1>
+  <PokemonPicture/>
+  <PokemonOptions/>
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
