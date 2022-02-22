4ca9999d57c556e59a7ad54fefb35eaa46b1c6ce
Danilo P Catalan <danilo.e.catalan@gmail.com>
2022-02-21 22:02:28 -0300

    add: components, axios and logic

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,17 +1,39 @@
 <template>
-  <h1>¿Quién es este pokemon?</h1>
-  <PokemonPicture/>
-  <PokemonOptions/>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quién es este pokemon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
+    <PokemonOptions :pokemons="pokemonArr"/>
+  </div>
 </template>
 
 <script>
 import PokemonOptions from '@/components/PokemonOptions.vue'
 import PokemonPicture from '@/components/PokemonPicture.vue'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
 
 export default {
   components: {
     PokemonPicture,
     PokemonOptions
+  },
+  data(){
+    return{
+      pokemonArr:[],
+      pokemon: null,
+      showPokemon: false
+    }
+  },
+  methods:{
+    async mixPokemonArray(){
+      this.pokemonArr = await getPokemonOptions()
+      const rndInt = Math.floor(Math.random() * 4)
+      this.pokemon = this.pokemonArr[rndInt]
+    }
+  },
+  mounted(){
+    this.mixPokemonArray()
   }
 }
 </script>
