d058a96d08a1b27f61c334ffc0f59776bf2a569e
Danilo P Catalan <danilo.e.catalan@gmail.com>
2022-02-21 23:04:11 -0300

    feature: add random selection and result

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -3,7 +3,16 @@
   <div v-else>
     <h1>¿Quién es este pokemon?</h1>
     <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
-    <PokemonOptions :pokemons="pokemonArr"/>
+    <PokemonOptions 
+      :pokemons="pokemonArr"
+      @selection="checkAnswer($event)"
+      />
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">
+        Nuevo juego
+      </button>
+    </template>
   </div>
 </template>
 
@@ -22,7 +31,9 @@ export default {
     return{
       pokemonArr:[],
       pokemon: null,
-      showPokemon: false
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     }
   },
   methods:{
@@ -30,6 +41,22 @@ export default {
       this.pokemonArr = await getPokemonOptions()
       const rndInt = Math.floor(Math.random() * 4)
       this.pokemon = this.pokemonArr[rndInt]
+    },
+    checkAnswer(selectedId){
+      this.showPokemon = true
+      this.showAnswer = true
+      if (selectedId === this.pokemon.id) {
+        this.message =`Correcto, ${this.pokemon.name}`
+      }else{
+        this.message =`Oops era, ${this.pokemon.name}`
+      }
+    },
+    newGame(){
+      this.pokemonArr = []
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemon = null
+      this.mixPokemonArray()
     }
   },
   mounted(){
